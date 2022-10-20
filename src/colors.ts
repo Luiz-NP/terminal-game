e4ba91e51db902532ed32f76856417bf0974b504
Luiz <gustavoooliveira111@gmail.com>
2022-10-20 01:05:48 -0300

    adding color palette

---
+++ src/colors.ts
@@ -0,0 +1,19 @@
+const red: string = "\x1b[31m";
+const green: string = "\x1b[32m";
+const yellow: string = "\x1b[33m";
+const blue: string = "\x1b[34m";
+const magenta: string = "\x1b[35m";
+const cyan: string = "\x1b[36m";
+const white: string = "\x1b[37m";
+const reset: string = '\u001b[0m';
+
+export {
+    red,
+    green,
+    yellow,
+    blue,
+    magenta,
+    cyan,
+    white,
+    reset
+}
