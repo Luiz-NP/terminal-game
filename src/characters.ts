b7ce46772662789b4f3ad30fee799c4158c4c992
Luiz <gustavoooliveira111@gmail.com>
2022-10-20 01:03:20 -0300

    adding properties to characters

--- src/characters.ts
+++ src/characters.ts
@@ -4,73 +4,84 @@ class Character {
     life: number;
     damage: number;
     ultimate: string;
+    moves: number;
 
     constructor(name: string,
                 type: string,
                 life: number,
                 damage: number,
-                ultimate: string,) {
+                ultimate: string,
+                moves: number) {
     this.name = name;
     this.type = type;
     this.life = life;
     this.damage = damage;
     this.ultimate = ultimate;
+    this.moves = moves;
     }
 }
 
 class Assassin extends Character {
     constructor(name: string,
-                type: string, 
-                ultimate: string, 
+                type: string,  
                 life: number, 
-                damage: number) {
+                damage: number,
+                ultimate: string,
+                moves: number) {
           super(name,
                 type, 
                 life, 
                 damage, 
-                ultimate);
+                ultimate,
+                moves);
     }
 }
 
 class Fighter extends Character {
     constructor(name: string,
                 type: string, 
-                ultimate: string, 
                 life: number, 
-                damage: number) {
+                damage: number,
+                ultimate: string,
+                moves: number) {
           super(name,
                 type, 
                 life, 
                 damage, 
-                ultimate);
+                ultimate,
+                moves);
     }
 }
 
 class Archer extends Character {
     constructor(name: string,
-                type: string, 
-                ultimate: string, 
+                type: string,  
                 life: number, 
-                damage: number) {
+                damage: number,
+                ultimate: string,
+                moves: number) {
           super(name,
                 type, 
                 life, 
                 damage, 
-                ultimate);
+                ultimate,
+                moves);
 }
 }
 
 class Mage extends Character {
     constructor(name: string,
-                type: string, 
-                ultimate: string, 
+                type: string,  
                 life: number, 
-                damage: number) {
+                damage: number,
+                ultimate: string,
+                moves: number) {
           super(name,
                 type, 
                 life, 
                 damage, 
-                ultimate);
+                ultimate,
+                moves);
 }
 }
 
